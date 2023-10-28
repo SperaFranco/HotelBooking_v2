0d39026c1baca11c0f2615245a1d578d51f04cad
Regino-Kamberaj <regino.kamberaj@stud.unifi.it>
2023-10-28 23:38:16 +0200

    Added some things i forgot to initial commit

---
+++ src/main/java/utilities/IdGenerator.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class IdGenerator {
+    //TODO classe statica che mi serve per generare (o Controllare)
+    // gli ID degli user alla loro registrazione
+    // gli ID delle prenotazioni
+    // gli ID delle carte di credito
+}
