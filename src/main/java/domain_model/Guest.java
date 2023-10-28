037842677abbce0340370dc91290ef607835e672
Regino-Kamberaj <regino.kamberaj@stud.unifi.it>
2023-10-29 00:35:00 +0200

    Writted some basic function for user, guest and hotelManager

--- src/main/java/domain_model/Guest.java
+++ src/main/java/domain_model/Guest.java
@@ -1,4 +1,28 @@
 package domain_model;
+import utilities.CreditCard;
+import utilities.User;
 
-public class Guest {
+import java.util.ArrayList;
+
+public class Guest extends User {
+    //Region fields
+    private ArrayList<Reservation> reservations;
+    private CreditCard card;
+    //end Region
+
+    public Guest(String id, String name, String surname, String email, String password, String telephone) {
+        super(id, name, surname, email, password, telephone);
+        this.reservations = new ArrayList<>();
+        this.card = null;
+    }
+
+    public CreditCard getCard() {
+        return card;
+    }
+
+    public void setCard(String cardHolderName, String cardNumber, String expiryDate, int CVV) {
+        this.card = new CreditCard(cardHolderName, cardNumber, expiryDate, CVV); //TODO rivedere il setCard
+        // --> magari durante le registrazioni nel caso di clienti gli viene chiesto di inserire le info della carta di credito
+
+    }
 }
--- src/main/java/utilities/CreditCard.java
+++ src/main/java/utilities/CreditCard.java
@@ -1,4 +1,76 @@
 package utilities;
 
+import java.util.Date;
+
 public class CreditCard {
+    //Region Fields
+    private String cardHolderName;
+    private String cardNumber;
+    private String expiryDate; //Data nel formato MM/YY
+    private int CVV; //Card verification number
+    private double balance = 1000.0; //saldo imposto per default il saldo degli utenti a 1000
+    //end Region
+
+
+    public CreditCard(String cardHolderName, String cardNumber, String expiryDate, int CVV) {
+        this.cardHolderName = cardHolderName;
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.CVV = CVV;
+
+        //TODO potrei controllore se la carta è valida tramite isValid altrimenti lanciare eccezione
+    }
+
+    //Region Getters and setters
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public void setCVV(int CVV) {
+        this.CVV = CVV;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //end Region
+
+    private boolean isValid(String cardNumber, String expiryDate, int CVV){
+        //TODO da implementare --> guardare metodi per verifica carte di credito
+        return false;
+    }
+
+    public void doPayment() {
+        //TODO il metodo va messo qui o nei guest??
+        //magari va fatto un controllo al saldo e se sufficiente esegui il pagamento
+    }
 }
